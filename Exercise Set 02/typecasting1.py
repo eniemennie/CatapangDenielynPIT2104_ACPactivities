8a0701e71139557a625ac4160a76168bd4ef86d5
Denielyn Catapang <denielyncatapang27@gmail.com>
2024-08-28 12:27:56 +0800

    Add files via upload

---
+++ Exercise Set 02/typecasting1.py
@@ -0,0 +1,6 @@
+def calculate_difference():
+    integer_input = float(input("Enter an integer: "))
+    float_input = float(input("Enter a float: "))
+    print(f"Difference: {integer_input - float_input}.")
+
+calculate_difference()
